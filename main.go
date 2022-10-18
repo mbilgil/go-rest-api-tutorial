ccc52119f859a602dbdaa44f19f0a9c09f558744
TCMBILGIL <mehmet.bilgil@turkcell.com.tr>
2022-10-18 09:50:18 +0300

    consuming a restful api

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
+
+type UniversityResponseModel []struct {
+	Domains       []string    `json:"domains"`
+	Country       string      `json:"country"`
+	StateProvince interface{} `json:"state-province"`
+	WebPages      []string    `json:"web_pages"`
+	Name          string      `json:"name"`
+	AlphaTwoCode  string      `json:"alpha_two_code"`
+}
 
 type Person struct {
 	ID        string   `json:"id,omitempty"`
@@ -58,14 +70,37 @@ func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
-func queryParamDisplayHandler(res http.ResponseWriter, req *http.Request) {
+func QueryParamDisplayHandler(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "query: "+req.FormValue("name"))
 	io.WriteString(res, "\nphone: "+req.FormValue("phone"))
 	println("Enter this in your browser:  http://localhost:9000/example?name=mehmet&phone=533-533")
-	// values := req.URL.Query()
-	// for k, v := range values {
-	// 	fmt.Println(k, " => ", v)
-	// }
+}
+
+func GetUniversityByCountry(w http.ResponseWriter, req *http.Request) {
+	url := "http://universities.hipolabs.com/search?country={COUNTRY_NAME}"
+	new_url := strings.ReplaceAll(url, "{COUNTRY_NAME}", req.FormValue("country"))
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", new_url, nil)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	req.Header.Add("Content-Type", "application/json")
+	//req.Header.Add("Authorization", "apikey 6WudO4PJvjsPyAAH8IVN8R:11ZbRiCFeSwryJ8xctO9O5")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	var responseObject UniversityResponseModel
+	json.Unmarshal(bodyBytes, &responseObject)
+	json.NewEncoder(w).Encode(responseObject)
+
 }
 
 func main() {
@@ -76,7 +111,7 @@ func main() {
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
 	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
-	router.HandleFunc("/example", queryParamDisplayHandler).Methods("GET")
-
+	router.HandleFunc("/example", QueryParamDisplayHandler).Methods("GET")
+	router.HandleFunc("/university", GetUniversityByCountry).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
