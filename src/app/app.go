0f88b25d93b2464dbd987e03fb8478ca0a10a198
TCMBILGIL <mehmet.bilgil@turkcell.com.tr>
2022-10-18 11:06:24 +0300

    file location editing

---
+++ src/app/app.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"go-rest-api-tutorial/src/example"
+	"go-rest-api-tutorial/src/person"
+	"go-rest-api-tutorial/src/university"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type App struct {
+	Router *mux.Router
+}
+
+func (a *App) Initialize() {
+	a.Router = mux.NewRouter()
+	a.setRouters()
+}
+
+// setRouters sets the all required routers
+func (a *App) setRouters() {
+	// Routing for handling the person
+	a.Get("/people/{id}", a.handleRequest(person.GetPersonEndpoint))
+	a.Post("/people", a.handleRequest(person.CreatePersonEndpoint))
+	a.Get("/people", a.handleRequest(person.GetPeopleEndpoint))
+	a.Delete("/people/{id}", a.handleRequest(person.DeletePersonEndpoint))
+
+	// Routing for handling university
+	a.Get("/university", a.handleRequest(university.GetUniversityByCountry))
+
+	// Routing for handling university
+	a.Get("/example", a.handleRequest(example.QueryParamDisplayHandler))
+
+}
+
+// Get wraps the router for GET method
+func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("GET")
+}
+
+// Post wraps the router for POST method
+func (a *App) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("POST")
+}
+
+// Put wraps the router for PUT method
+func (a *App) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("PUT")
+}
+
+// Delete wraps the router for DELETE method
+func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("DELETE")
+}
+
+func (a *App) Run(host string) {
+	log.Fatal(http.ListenAndServe(host, a.Router))
+}
+
+type RequestHandlerFunction func(w http.ResponseWriter, r *http.Request)
+
+func (a *App) handleRequest(handler RequestHandlerFunction) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r)
+	}
+}
